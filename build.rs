e3e14a801c9cea334cb6f7bb43dc07cb9fdbffc0
JustSailing <>
2025-09-22 20:02:07 -0400

    simplified build.rs and scripts

--- build.rs
+++ build.rs
@@ -1,26 +1,18 @@
 use std::process::Command;
 
 fn main() {
-    compile_shaders();
     println!("cargo:rustc-link-lib=X11");
-    println!("cargo:rustc-link-lib=vulkan")
+    println!("cargo:rustc-link-lib=vulkan");
+    compile_shaders();
+    println!("cargo:rerun-if-changed=bin/assets/shaders");
+    println!("cargo:rerun-if-changed=assets/shaders");
 }
 
 fn compile_shaders() {
     let bash_script_path = "compile_shaders.sh";
 
-    // Execute the bash script using sh or bash
-    #[cfg(debug_assertions)]
     let output = Command::new("bash") // Or "bash"
         .arg(bash_script_path)
-        .arg("debug")
-        .status() // Use .status() if you only need the exit code, or .output() for stdout/stderr
-        .expect("Failed to execute bash script");
-
-    #[cfg(not(debug_assertions))]
-    let output = Command::new("bash") // Or "bash"
-        .arg(bash_script_path)
-        .arg("release")
         .status() // Use .status() if you only need the exit code, or .output() for stdout/stderr
         .expect("Failed to execute bash script");
 
--- compile_shaders.sh
+++ compile_shaders.sh
@@ -9,38 +9,20 @@ for vert_shader in "$SHADER_DIR"/*.vert.glsl; do
   if [ -f "$vert_shader" ]; then
     filename=$(basename -- "$vert_shader")
     filename_no_ext="${filename%.*}"
-    if [ "$1" != "debug" ]; then
-      output_dir="bin/assets/shaders"
-      output_path="$output_dir/$filename_no_ext.spv"
-      echo $output_path
-      if [ -f "$output_path" ]; then
-        echo "file exists: $output_path"
-      else
-        echo "creating file: $output_path"
-        touch "$output_path"  
-      fi
-      echo "  Compiling $vert_shader to $output_path"
-      glslc -fshader-stage=vertex "$vert_shader" -o "$output_path"
-      if [ $? -ne 0 ]; then
-        echo "Error compiling $vert_shader"
-        exit 1
-      fi
+    output_dir="bin/assets/shaders"
+    output_path="$output_dir/$filename_no_ext.spv"
+    echo $output_path
+    if [ -f "$output_path" ]; then
+      echo "file exists: $output_path"
     else
-      output_dir="bin/assets/shaders"
-      output_path="$output_dir/$filename_no_ext.spv"
-      echo $output_path
-      if [ -f "$output_path" ]; then
-        echo "file exists: $output_path"
-      else
-        echo "creating file: $output_path"
-         touch "$output_path"  
-      fi
-      echo "  Compiling $vert_shader to $output_path"
-      glslc -fshader-stage=vertex "$vert_shader" -o "$output_path"
-      if [ $? -ne 0 ]; then
-        echo "Error compiling $vert_shader"
-        exit 1
-      fi
+      echo "creating file: $output_path"
+       touch "$output_path"  
+    fi
+    echo "  Compiling $vert_shader to $output_path"
+    glslc -fshader-stage=vertex "$vert_shader" -o "$output_path"
+    if [ $? -ne 0 ]; then
+      echo "Error compiling $vert_shader"
+      exit 1
     fi
   fi
 done
@@ -50,36 +32,19 @@ for frag_shader in "$SHADER_DIR"/*.frag.glsl; do
   if [ -f "$frag_shader" ]; then
     filename=$(basename -- "$frag_shader")
     filename_no_ext="${filename%.*}"
-    if [ "$1" != "debug" ]; then
-      output_dir="bin/assets/shaders"
-      output_path="$output_dir/$filename_no_ext.spv"
-      if [ -f "$output_path" ]; then
-        echo "file exists: $output_path"
-      else
-        echo "creating file: $output_path"
-         touch "$output_path"  
-      fi
-      echo "  Compiling $frag_shader to $output_path"
-      glslc -fshader-stage=fragment "$frag_shader" -o "$output_path"
-      if [ $? -ne 0 ]; then
-        echo "Error compiling $frag_shader"
-        exit 1
-      fi
+    output_dir="bin/assets/shaders"
+    output_path="$output_dir/$filename_no_ext.spv"
+    if [ -f "$output_path" ]; then
+      echo "file exists: $output_path"
     else
-      output_dir="bin/assets/shaders"
-      output_path="$output_dir/$filename_no_ext.spv"
-      if [ -f "$output_path" ]; then
-        echo "file exists: $output_path"
-      else
-        echo "creating file: $output_path"
-         touch "$output_path"  
-      fi
-      echo "  Compiling $frag_shader to $output_path"
-      glslc -fshader-stage=fragment "$frag_shader" -o "$output_path"
-      if [ $? -ne 0 ]; then
-        echo "Error compiling $frag_shader"
-        exit 1
-      fi
+      echo "creating file: $output_path"
+      touch "$output_path"  
+    fi
+    echo "  Compiling $frag_shader to $output_path"
+    glslc -fshader-stage=fragment "$frag_shader" -o "$output_path"
+    if [ $? -ne 0 ]; then
+      echo "Error compiling $frag_shader"
+      exit 1
     fi
   fi
 done
