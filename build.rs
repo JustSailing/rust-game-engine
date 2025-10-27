3d503ef7ec37480ee39bf84575cf4ea949b24677
JustSailing <william.j.haddad98@gmail.com>
2025-10-27 13:37:16 -0400

    changed linker to mold

--- build.rs
+++ build.rs
@@ -2,6 +2,7 @@ use core::panic;
 use std::process::Command;
 
 fn main() {
+    println!("cargo:rustc-link-arg=-fuse-ld=mold");
     println!("cargo:rustc-link-lib=X11");
     println!("cargo:rustc-link-lib=vulkan");
     compile_shaders();
@@ -12,15 +13,14 @@ fn main() {
 fn compile_shaders() {
     let bash_script_path = "compile_shaders.sh";
 
-    let output = Command::new("bash") // Or "bash"
+    let output = Command::new("bash")
         .arg(bash_script_path)
-        .status() // Use .status() if you only need the exit code, or .output() for stdout/stderr
+        .status()
         .expect("Failed to execute bash script");
 
-    // Check if the command executed successfully
+
     if !output.success() {
         eprintln!("Bash script execution failed!");
-        // You might want to panic! here to stop the build
         panic!()
     }
 }
